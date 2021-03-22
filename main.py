4bed96ab128f24d626ac644e8d5c56709bc2afdb
Arthur Holanda <holandaarthur46@gmail.com>
2021-03-22 17:40:02 -0300

    Programa Final

--- main.py
+++ main.py
@@ -3,17 +3,16 @@ print('''Digite uma opção abaixo:
 1-Para cadastrar Animais;
 2-Para Funcionarios;
 3-Para Produtos;''')
+
 nomes = []
 especies = []
 raças = []
 nascimento = []
 
-
 opção = int(input())
 if opção == 1:
-
     for c in range(0,2):
-
+        print('***Cadastro de Animais***')
         print('-' * 25)
         animal.nome = input(str('Digite  o nome do seu Pet: '))
         nomes.append(animal.nome)
@@ -27,23 +26,29 @@ if opção == 1:
         animal.especie = input(str('Digite a especia do seu Pet: '))
         especies.append(animal.especie)
         print('-'*25)
-        print('##Dados_do_seu_Bixinho##')
+        print('    ##Dados do seu Bixinho##    ')
         print(animal.verDados(animal))
         print(f'Idade do animal:{animal.idade(animal)}')
 
+    print('-' * 35 )
+
     print(f'''
-    +++ Dados_Animais +++
+     +++ Dados_Animais +++
+    
     Nome dos animais:{nomes}
     Raça dos animais:{raças}
     Especie dos animais:{especies}
               ''')
 
+    print('-' * 35)
+
 from petshop import Funcionario
+
 nomes3 = []
 função1 = []
+
 if opção == 2:
     for z in range(0,2):
-
         print('***Cadastro de Funcionarios***')
         Funcionario.nome = input(str('Digite o nome: '))
         nomes3.append(Funcionario.nome)
@@ -56,13 +61,16 @@ if opção == 2:
         print('-' * 30)
         print(f'Salario do Funcionario = R${Funcionario.salario(Funcionario)}')
         print('-' * 30)
+
     print(f'''
-        +++Nome_dos_Funcionarios_e_suas_Funções+++
+         +++Nome dos Funcionarios e suas Funções+++
+        
         Nomes:{nomes3}
         Funções:{função1}''')
-from petshop import Produto
-if opção == 3:
 
+from petshop import Produto
+
+if opção == 3:
 
     marcas = []
     nomes1 = []
@@ -71,6 +79,7 @@ if opção == 3:
     total = []
 
     for x in range(0,2):
+        print('***Cadastro de Produtos***')
         print('-' * 25)
         nomes1.append(input(str('Digite o nome do produto: ')))
         print('-' * 25)
@@ -78,16 +87,12 @@ if opção == 3:
         print('-' * 25)
         Produto.preço = int(input('Digite o preço: '))
         preços.append(Produto.preço)
-        #preços.append(float(input('Digite o preço do produto: ')))
         print('-' * 25)
         Produto.quantidade = int(input('Digite a quantidade: '))
         quantidades.append(Produto.quantidade)
-        #quantidade.append(int(input('Digite a quantidade: ')))
         print('-' * 25)
 
         Produto.quantidadeGeral = sum(quantidades)
-
-
 
         print(f'''
         +++Dados_Produtos+++
@@ -95,6 +100,7 @@ if opção == 3:
         Preços:{preços}
         Quantidade:{quantidades}
         ''')
+
     print(f'Valor total em estoque:R${Produto.VTEE(Produto)}')
     print('-' * 25)
 
